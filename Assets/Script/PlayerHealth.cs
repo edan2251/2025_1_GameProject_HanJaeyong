dfe38f538021b7b26b6bad3930f024bbc7676988
edan <molphone0328@gmail.com>
2025-04-07 15:50:28 +0900

    [Feat] 프리팹 자동생성과 충돌처리 및 빌드

---
+++ Assets/Script/PlayerHealth.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxLives = 3;
+    public int currentLives = 1;
+
+    public float invincibleTime = 1.0f;
+    public bool isInvincible = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLives = maxLives;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Missile"))
+        {
+            currentLives--;
+            Destroy(other.gameObject);
+
+            if(currentLives <= 0)
+            {
+                GameOver();
+            }
+
+
+        }
+    }
+
+    public void GameOver()
+    {
+        gameObject.SetActive(false);
+        Invoke("RestartGame", 3.0f);        //몇초 후에 "" 안의 함수 발동
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene() .name);
+    }
+}
